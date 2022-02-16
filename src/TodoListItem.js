75c7fef303370860a5ba252b8b04e4f85df87649
Hermes Vierma <inghermesvierma@hotmail.com>
2022-02-16 15:41:13 -0600

    Lesson-3-1

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './TodoListItem.module.css'
 
 function TodoListItem(props) {
   const { todo } = props;
 
   return (
-    <li>
+    <li className={style.ListItem}>
       { todo.fields.Title }
       <button type="button" onClick={()=>{props.onRemoveTodo(todo.id)}}>Remove</button>
     </li>
